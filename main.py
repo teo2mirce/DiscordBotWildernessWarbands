103c73b781162b2c6c2f2b817c4bd164b39a3e9f
teo2mirce <teo2mircea@gmail.com>
2021-01-26 22:31:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from tabulate import tabulate
 from keep_alive import keep_alive
 import time
 
-hed=["Worlds","DWF","ELM","RDI","Unknown"]
+hed=["","DWF","ELM","RDI"]
 client = discord.Client()
 
-dicSkill=dict()
-dicLoc=dict()
-dicBroken=dict()
-dicBeam=dict()
+dickSkill=dict()
+dickLoc=dict()
+dickBroken=dict()
+dickBeam=dict()
+dickDead=dict()
 
 
 def Skill2Index(Skills):
@@ -32,24 +33,28 @@ def Skill2Index(Skills):
     
 def BeamBroke(i):
   toret=str(i)
-  if i in dicBeam:
-    toret= toret+' (Beamed)'
-  if i in dicBroken:
-    toret= toret+' (Broken)'
+  if i in dickBeam:
+    toret= '*'+toret+'*'
+  if i in dickBroken:
+    toret= toret+' B'
+  if i in dickDead:
+    toret= toret+' D'
   return "\n"+toret
 def pepperTheTable():
   global table
-  table = [["Herblore","","","",""],["Farming","","","",""],["Construction","","","",""],["Smithing","","","",""],["Mining","","","",""],["Unknown","","","",""]]
+  table = [["H","","",""],["F","","",""],["C","","",""],["S","","",""],["M","","",""],["?","","",""]]
   for i in range(200):
-    if i in dicLoc and i in dicSkill:
-      for id in Skill2Index(dicSkill[i]):
-        table[id][hed.index(dicLoc[i])]+=BeamBroke(i)
+    #3 cazuri
+    if i in dickLoc and i in dickSkill:#ambele
+      for id in Skill2Index(dickSkill[i]):
+        table[id][hed.index(dickLoc[i])]+=BeamBroke(i)
       continue
-    if i in dicLoc:
-      table[5][hed.index(dicLoc[i])]+=BeamBroke(i)
-    if i in dicSkill:
-      for id in Skill2Index(dicSkill[i]):
-        table[id][4]+=BeamBroke(i)
+    if i in dickLoc:#doar locatie
+      table[5][hed.index(dickLoc[i])]+=BeamBroke(i)
+    if i in dickSkill:#doar skill
+      for id in Skill2Index(dickSkill[i]):
+        break
+        #table[id][4]+=BeamBroke(i)
 def updateTab(arr):
   if len(arr)<2 or len(arr) > 3:
     return
@@ -63,25 +68,37 @@ def updateTab(arr):
 
 
     if arr[1] == "BROKE" or arr[1] == "BROKEN":
-      dicBroken[world]=True
+      dickBroken[world]=True
+      dickBeam.pop(world, None)
+      dickDead.pop(world, None)
       return
 
     if arr[1] == "BEAM" or arr[1] == "BEAMED":
-      dicBeam[world]=True
+      dickBeam[world]=True
+      dickBroken.pop(world, None)
+      dickDead.pop(world, None)
       return
 
     if arr[1] == "DEAD":
-      dicLoc.pop(world, None)
-      dicSkill.pop(world, None)
-      dicBroken.pop(world, None)
-      dicBeam.pop(world, None)
+      dickSkill.pop(world, None)
+      dickDead[world]=True
+      dickBeam.pop(world, None)
+      dickBroken.pop(world, None)
+      return
+    
+    if arr[1] == "DELETE" or arr[1] == "CLEAR" or arr[1] == "REMOVE":
+      dickLoc.pop(world,None)
+      dickSkill.pop(world, None)
+      dickDead.pop(world,None)
+      dickBeam.pop(world, None)
+      dickBroken.pop(world, None)
       return
 
-    if arr[1] in ["DWF","ELM","RDI"]:
-      dicLoc[world]=arr[1]
-    else:
+    if arr[1] in ["DWF","ELM","RDI"]:#a zis locatie deci skill unknown
+      dickLoc[world]=arr[1]
+    else:#a zis skill deci locatie unknown
       if Skill2Index(arr[1])!=[]:
-        dicSkill[world]=arr[1]
+        dickSkill[world]=arr[1]
   if len(arr)==3:
     loc=arr[1].upper()
     skill=arr[2].upper()
@@ -89,9 +106,10 @@ def updateTab(arr):
       loc=arr[2].upper()
       skill=arr[1].upper()
     if loc not in ["DWF","ELM","RDI"]:
+      print(loc,' nu e locatie')
       return
-    dicLoc[world]=loc
-    dicSkill[world]=skill
+    dickLoc[world]=loc
+    dickSkill[world]=skill
 
 
 @client.event
@@ -99,7 +117,7 @@ async def on_ready():
     global table
     global timeRestart
     timeRestart=0
-    table = [["Herblore","","","",""],["Farming","","","",""],["Construction","","","",""],["Smithing","","","",""],["Mining","","","",""],["Unknown","","","",""]]
+    table = [["H","","",""],["F","","",""],["C","","",""],["S","","",""],["M","","",""],["?","","",""]]
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
@@ -111,33 +129,43 @@ async def on_message(message):
     msg=message.content
 
     if int(time.time()/3600)!=timeRestart:
-      dicLoc.clear()
-      dicSkill.clear()
-      dicBroken.clear()
-      dicBeam.clear()
+      dickLoc.clear()
+      dickSkill.clear()
+      dickBroken.clear()
+      dickBeam.clear()
+      dickDead.clear()
       timeRestart=int(time.time()/3600)
 
     if ' ' in msg:
       arr=msg.split(' ')
       updateTab(arr)
+    #https://stackoverflow.com/questions/52241051/i-want-to-let-my-discord-bot-send-images-gifs
+    if msg=='Rick me':
+      await message.channel.send(file=discord.File('tenor.gif'))
 
     if msg=='cLeAr eVeRyThInG':
-      dicLoc.clear()
-      dicSkill.clear()
-      dicBroken.clear()
-      dicBeam.clear()
+      dickLoc.clear()
+      dickSkill.clear()
+      dickBroken.clear()
+      dickBeam.clear()
+      dickDead.clear()
 
     if msg=='s':
         pepperTheTable()
-        outy = ("```" + "\n\n" + tabulate(table, tablefmt="fancy_grid", headers=hed) + "```")
+        outy = ("```" + tabulate(table, tablefmt="fancy_grid", headers=hed) + "```")
         await message.channel.send(outy)
 tok=os.getenv('TOKEN')
 keep_alive()
 client.run(tok)
 
 
+
+#35 broken = 35 apare taiat
+#35 dead = 35 dispare din tabel de peste tot
+#35 beamed = 35 apare cu bold si subliniat
 #https://stackoverflow.com/questions/25244454/python-create-strikethrough-strikeout-overstrike-string-type/25244576
 #my_dict.pop('key', None)
+
 #https://stackoverflow.com/questions/57582873/posting-embedded-output-using-tabulate-and-discord-rewrite
 #https://pypi.org/project/tabulate/
 #https://stackoverflow.com/questions/9535954/printing-lists-as-tabular-data
